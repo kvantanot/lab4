670c4e8e964ef1f4d46d2765d2f65bbc25b31cde
Sergey <kvapuluk71@gmail.com>
2020-05-01 22:53:11 +0300

    Add table output

---
+++ lab4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[4];
+            employees[0] = new Employee() { name = "Вася Пупкин", company = "Яблоко", hireDate = new DateTime(2015, 7, 12), salesSum = 5623.5m, currency = '€' };
+            employees[1] = new Employee() { name = "Анатолий Едкий", company = "ООО Груша", hireDate = new DateTime(2020, 2, 29), salesSum = 950m, currency = '€' };
+            employees[2] = new Employee() { name = "Геннадий Ковбойский", company = "Телемагазин", hireDate = new DateTime(2014, 5, 1), salesSum = 19000500m, currency = '₽' };
+            employees[3] = new Employee() { name = "Екатерина Змеиная", company = "Яд и копыта", hireDate = new DateTime(2011, 12, 1), salesSum = 2140954352.99m, currency = '₽' };
+            Console.OutputEncoding = Encoding.Unicode;
+            for (int i = 0; i < 4; i++)
+            {
+                employees[i].Write();
+
+            }
+        }
+
+    }
+
+    public class Employee
+    {
+        public String name;
+        public String company;
+        public DateTime hireDate;
+        public Decimal salesSum;
+        public char currency;
+
+        public void Write()
+        {
+            Console.WriteLine("{0,25} | {1,25} | {2:d} | {3}{4}", name, $"\"{company}\"", hireDate, salesSum, currency);
+        }
+    }
+}
